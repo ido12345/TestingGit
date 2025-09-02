f1e7678bb4d6d15d3f5e5168cae9b0280da256d5
ido <idosweed121@gmail.com>
2025-09-02 13:25:31 +0300

    Changed Message

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace MyApp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello C#!");
         }
     }
 }
